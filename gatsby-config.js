7eb9d33370206e99463c0a6021ab8b23be393f9c
Hussain4398 <101874906+Hussain4398@users.noreply.github.com>
2022-03-24 14:25:45 +0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,17 +52,4 @@ module.exports = {
     },
   ],
 };
-module.exports = {
-  siteMetadata: {
-    title: "My Gatsby Site",
-  },
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `markdown-pages`,
-        path: `${__dirname}/src/markdown-pages`,
-      },
-    },
-  ],
-}
+
